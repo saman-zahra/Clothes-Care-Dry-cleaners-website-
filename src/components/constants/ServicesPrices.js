0cab0427001f3b89054b308e928b8e01cf36f6fe
saman-zahra <122096807+saman-zahra@users.noreply.github.com>
2026-01-07 11:19:55 +0500

    Add files via upload

---
+++ src/components/constants/ServicesPrices.js
@@ -0,0 +1,267 @@
+export const DryCleaningPrices = [
+  { item: "Two piece suit", price: 14.99 },
+  { item: "Three piece suit", price: 19.5 },
+  { item: "Dinner suit", price: 16.5 },
+  { item: "Dinner suit three piece", price: 21.99 },
+  { item: "Trouser", price: 8.5 },
+  { item: "Jacket", price: 9.99 },
+  { item: "Jacket feather", price: 20 },
+  { item: "Coat long", price: 18 },
+  { item: "Coat short", price: 16 },
+  { item: "Feather coat", price: 25 },
+  { item: "Jumper", price: 8.5 },
+  { item: "Cardigan", price: 8.5 },
+  { item: "Knitwear", price: 8.5 },
+  { item: "Dress short", price: 12.95 },
+  { item: "Dress 3/4", price: 14.99 },
+  { item: "Dress long", price: 18 },
+  { item: "Jumpsuit", price: 18 },
+  { item: "Skirt short", price: 6.5 },
+  { item: "Skirt long", price: 8.95 },
+  { item: "Waist coat", price: 7.5 },
+  { item: "Top silk", price: 8.5 },
+  { item: "Top", price: 7.5 },
+  { item: "Blouse", price: 7.5 },
+  { item: "Shorts", price: 6.5 },
+  { item: "Scarf", price: 6.5 },
+  { item: "Pashmina", price: 8.5 },
+  { item: "Gown", price: 13.89 },
+  { item: "Jeans", price: 7.5 },
+  {
+    item: "Children's items (dry clean)",
+    price: "£0.00(children’s items will bế invoice at 65% of aduit prices)",
+  },
+];
+
+export const ShirtsServicePrices = [
+  { item: "Shirts hang", price: 3 },
+  { item: "Shirt fold", price: 3.5 },
+  { item: "Shirt dry clean", price: 4.99 },
+  { item: "Ladies shirt", price: 3.5 },
+  { item: "Shirt press only", price: 2 },
+];
+
+export const LaundryServicePrices = [
+  { item: "Blouse - wash & press", price: 6.35 },
+  { item: "Bra - wash", price: 2.35 },
+  { item: "Children items - wash & press", price: 0 },
+  { item: "Coat - wash & press", price: 16.9 },
+  { item: "Dress - wash & press", price: 12.2 },
+  { item: "Jacket - wash & press", price: 9.5 },
+  { item: "Jeans - wash & press", price: 6.9 },
+  { item: "Jumper - wash & press", price: 7.4 },
+  { item: "Knitwear - wash & press", price: 7.95 },
+  { item: "Polo shirt - wash & press", price: 4.15 },
+  { item: "Pyjamas - wash & press", price: 8.5 },
+  { item: "Shorts - wash & press", price: 5.25 },
+  { item: "Sweatshirt (hooded) - wash & press", price: 7.95 },
+  { item: "Top - wash & press", price: 6.9 },
+  { item: "Trousers - wash & press", price: 6.9 },
+  { item: "T-shirt - wash & press", price: 3.5 },
+  { item: "Vest - wash & press", price: 2.95 },
+];
+
+export const SkiSportswearPrices = [
+  { item: "Ski gloves", price: 12.95 },
+  { item: "Ski jacket", price: 25 },
+  { item: "Ski trousers", price: 12.85 },
+  { item: "Ski suit", price: 25 },
+  { item: "Rugby shirt", price: 4.99 },
+  { item: "Rugby shorts", price: 5.95 },
+  { item: "Socks", price: 3 },
+  { item: "Tracksuit", price: 12.95 },
+  { item: "Track top", price: 7.95 },
+];
+
+export const HouseholdPrices = [
+  { item: "Table cloth", price: 9.5 },
+  { item: "Napkin", price: 1.5 },
+  { item: "Kitchen cloth", price: 1 },
+  { item: "Face towel", price: 1 },
+  { item: "Hand towel", price: 1 },
+  { item: "Bath towel", price: 3 },
+  { item: "Oven cloth", price: 1.5 },
+  { item: "Bath mat", price: 4.5 },
+  { item: "Bath sheet", price: 5 },
+  { item: "Mattress cover single", price: 16 },
+  { item: "Mattress cover double", price: 20 },
+  { item: "Mattress cover king size", price: 22 },
+  { item: "Mattress topper", price: 35 },
+];
+
+export const FurnitureCoverPrices = [
+  { item: "Armchair cover", price: 5.5 },
+  { item: "Armrest cover", price: 5.5 },
+  { item: "Sofa cover", price: 9.5 },
+  { item: "Stool cover", price: 5.5 },
+  { item: "Seat cover", price: 5.5 },
+  { item: "Cushion cover", price: 6.5 },
+  { item: "Table cloth", price: 9 },
+];
+
+export const DuvetAndBedLinenPrices = [
+  {
+    item: "Bed set - 1 double duvet cover, 1 double sheet, 2 pillowcases (wash & press)",
+    price: 15.99,
+  },
+  {
+    item: "Bed set - 1 king duvet cover, 1 king sheet, 2 pillow cases (wash & press)",
+    price: 16.99,
+  },
+  {
+    item: "Bed set - 1 single sheet, 1 single duvet cover, 1 pillow case (wash & press)",
+    price: 14.85,
+  },
+  {
+    item: "Bed set - 1 super king duvet cover, 1 super king sheet, 2 pillowcase (wash & press)",
+    price: 18.99,
+  },
+  {
+    item: "Bed set (silk) - 1 king duvet cover, 1 king sheet, 2 pillow cases",
+    price: 25,
+  },
+  { item: "Bed spread (heavy)", price: 25 },
+  { item: "Bed throw", price: 25 },
+  { item: "Blanket", price: 20 },
+  { item: "Duvet cover (double) - press only", price: 6 },
+  { item: "Duvet cover (double) - wash & press", price: 8.5 },
+  { item: "Duvet cover (king) - press only", price: 6.5 },
+  { item: "Duvet cover (king) - wash & press", price: 8.95 },
+  { item: "Duvet cover (single) - press only", price: 6 },
+  { item: "Duvet cover (single) - wash & press", price: 8 },
+  { item: "Duvet cover (super king) - press only", price: 7.5 },
+  { item: "Duvet cover (super king) - wash & press", price: 9.5 },
+  { item: "Duvet cover (super king) - silk", price: 12.5 },
+  { item: "Duvet (double)", price: 24.99 },
+  { item: "Duvet (double) - feather", price: 27 },
+  { item: "Duvet (king)", price: 27 },
+  { item: "Duvet (king) - feather", price: 29.5 },
+  { item: "Duvet (single)", price: 22 },
+  { item: "Duvet (single) - feather", price: 24.99 },
+  { item: "Duvet (super king)", price: 26.5 },
+  { item: "Duvet (super king) - feather", price: 29.99 },
+  { item: "Mattress protector", price: 19.99 },
+  { item: "Mattress topper", price: 28.1 },
+  { item: "Mattress topper - feather", price: 35 },
+  { item: "Pillow", price: 11 },
+  { item: "Pillow case - press only", price: 1.85 },
+  { item: "Pillow case - silk", price: 4 },
+  { item: "Pillow case - wash & press / dry clean", price: 2.5 },
+  { item: "Sheet (double) - press only", price: 5.5 },
+  { item: "Sheet (double) - wash & press", price: 6.5 },
+  { item: "Sheet (king) - press only", price: 5.5 },
+  { item: "Sheet (king) - wash & press", price: 6.95 },
+  { item: "Sheet (single) - press only", price: 5.5 },
+  { item: "Sheet (single) - wash & press", price: 6.5 },
+  { item: "Sheet (super king) - press only", price: 6 },
+  { item: "Sheet (super king) - wash & press", price: 7.5 },
+];
+
+export const CurtainCleaningPrices = [
+  { item: "Curtains - blackout/thermal (per m²) - cleaning", price: 15.35 },
+  { item: "Curtains (heavy lined - per m²) - cleaning", price: 8.45 },
+  { item: "Curtains (non-lined - per m²) - cleaning", price: 5.4 },
+  { item: "Curtains (medium lined - per m²) - cleaning", price: 6.9 },
+];
+
+export const WeddingDressCleaningPrice = [
+  { item: "Wedding dress cleaning (starting from)", price: 85 },
+];
+
+export const ServiceWashPrice = [
+  { item: "Wash only", price: "To be determined based on the specific items" },
+];
+
+export const AlterationsAndRepairPrices = [
+  { item: "Button repair", price: 3.55 },
+  { item: "Dress (1 layer) - shortening", price: 21.85 },
+  { item: "Dress (pleated/multiple layers) - shortening", price: 35 },
+  { item: "New zip (0” to 10”)", price: 17.5 },
+  { item: "New zip (10” to 30”)", price: 33.4 },
+  { item: "New zip (30”+)", price: 40 },
+  { item: "Patch repair", price: 16.4 },
+  { item: "Re-hem trousers (1 leg)", price: 7.5 },
+  { item: "Skirt (1 layer) - shortening", price: 16 },
+  { item: "Skirt (pleated/multiple layers) - shortening", price: 20 },
+  { item: "Trouser shortening", price: 16 },
+  { item: "Trouser waist in / waist out", price: 16 },
+];
+
+export const services = [
+  {
+    name: "Dry Cleaning",
+    description:
+      "Professional care for delicate fabrics, ensuring garments receive meticulous cleaning and attention to detail.",
+    ServiceDetail: [...DryCleaningPrices],
+  },
+  {
+    name: "Shirts Service",
+    description:
+      "Tailored cleaning process for shirts, maintaining quality, crispness, and professional appearance.",
+    ServiceDetail: [...ShirtsServicePrices],
+  },
+  {
+    name: "Laundry",
+    description:
+      "Comprehensive cleaning service for everyday clothing, linens, and towels, ensuring freshness and cleanliness.",
+    ServiceDetail: [...LaundryServicePrices],
+  },
+  {
+    name: "Ski Sportswear",
+    description:
+      "Specialized cleaning and maintenance for ski gear, preserving performance and durability for slopes.",
+    ServiceDetail: [...SkiSportswearPrices],
+  },
+  {
+    name: "Household",
+    description:
+      "Refreshing household items including cushions, rugs, and drapes with meticulous cleaning care.",
+    ServiceDetail: [...HouseholdPrices],
+  },
+  {
+    name: "Furniture Covers",
+    description:
+      "Revitalizing furniture covers with professional cleaning, ensuring vibrancy and longevity for furnishings.",
+    ServiceDetail: [...FurnitureCoverPrices],
+  },
+  {
+    name: "Leather, Fur and Suede",
+    description:
+      "Expert cleaning for luxury items, preserving texture and appearance of leather, fur, and suede.",
+  },
+  {
+    name: "Curtains",
+    description:
+      "Gentle yet effective cleaning process to remove dust, dirt, and stains, restoring curtain beauty.",
+    ServiceDetail: [...CurtainCleaningPrices],
+  },
+  {
+    name: "Wedding Dresses",
+    description:
+      "Preserving wedding gown beauty with specialized cleaning, ensuring pristine condition and sentimental value.",
+    ServiceDetail: [...WeddingDressCleaningPrice],
+  },
+  {
+    name: "Service Wash",
+    description:
+      "Convenient laundry service, providing clean clothes without the hassle of washing and drying.",
+    ServiceDetail: [...ServiceWashPrice],
+  },
+  {
+    name: "Duvet and bed linen",
+    description:
+      "Restful sleep with fresh duvet and linen cleaning, removing dirt and allergens.",
+    ServiceDetail: [...DuvetAndBedLinenPrices],
+  },
+  {
+    name: "Alterations and Repair",
+    description:
+      "Skilled tailoring for garment alterations and repairs, ensuring the perfect fit and craftsmanship.",
+    ServiceDetail: [...AlterationsAndRepairPrices],
+  },
+  {
+    name: "Specialist and Delicate items",
+    description:
+      "Meticulous care for delicate fabrics and intricate designs, providing specialized cleaning attention.",
+  },
+];
